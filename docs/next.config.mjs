da9be0efcab54e3318385fdc08634206a608e66a
webtheory <webtheory@mail.ru>
2025-10-30 12:38:47 +0200

    Update docs config, deploy script, and repository URL

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -24,6 +24,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? `/${repository}` : undefined,
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repository}` : undefined,
   pageExtensions: ['ts', 'tsx', 'mdx'],
   eslint: {
     ignoreDuringBuilds: true,
